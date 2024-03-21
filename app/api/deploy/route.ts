aea9148b59710e471701df0e75a888898b01d6f2
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-21 18:24:09 +0530

    clone repo

--- app/api/deploy/route.ts
+++ app/api/deploy/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import simpleGit from "simple-git";
+import { v4 as uuidv4 } from 'uuid';
 
 export const GET = async () => {
     return new NextResponse(JSON.stringify({ message: "Hello World" }));
@@ -8,6 +10,12 @@ export const POST = async (request: NextRequest) => {
     const body = await request.json();
     const { repoUrl } = body;
     if (!repoUrl) return new NextResponse(JSON.stringify({ message: "Missing repoUrl" }), { status: 400 });
+    try {
+        const id = uuidv4();
+        await simpleGit().clone(repoUrl, `/repos/${id}`);
+    } catch (e) {
+        return new NextResponse(JSON.stringify({ message: "Error cloning repo" }), { status: 500 });
+    }
     return new NextResponse(JSON.stringify({ message: "Hello World", body }));
 };
 
